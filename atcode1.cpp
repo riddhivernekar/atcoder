150068dfc14530609f7fec1b29159711732307a1
Riddhi Vernekar <108002090+riddhivernekar@users.noreply.github.com>
2022-12-04 12:32:27 +0530

    Add files via upload
    https://atcoder.jp/contests/abc280/tasks/abc280_a

---
+++ atcode1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t,n;
+  int sum=0;
+  char a[n];
+  cin>>t>>n;
+  for(int i=0;i<t;i++)
+  {
+      for(int j=0;j<n;j++)
+      {cin>>a[j];
+          if(a[j]=='#'){sum++;}
+      }
+  }
+  cout<<sum<<endl;
+    
+	return 0;
+}
